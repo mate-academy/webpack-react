0b9a93a1c8fc3fcbbf0b3632937d09295aaf617a
mgrinko <glukym@gmail.com>
2019-02-18 09:48:45 +0200

    WDS was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js',
+  entry: {
+    'index': './src/index.js'
+  },
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name][hash].js'
   },
 
+  devtool: 'source-map',
+
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(['dist'])
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 };
